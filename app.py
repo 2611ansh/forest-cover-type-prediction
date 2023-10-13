62057a3eed51d0d8b2168d4adb08c93e2563997a
Ansh Raghuvanshi <121013240+2611ansh@users.noreply.github.com>
2023-10-13 17:03:17 +0000

    prediction with accuracy of 100%

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import pickle
 import pandas as pd
 from flask import Flask, render_template, request
-from sklearn.preprocessing import StandardScaler  # Import StandardScaler for clarity
 
 # Load the model and scaler
 model = pickle.load(open('artifacts/model.pkl', 'rb'))
-scaler = pickle.load(open('artifacts/scaler.pkl', 'rb'))
 
 app = Flask(__name__)
 
@@ -20,12 +18,8 @@ def predict():
             # Ensure that the form data keys match your model's input features
             data = {key: [value] for key, value in request.form.items() if key != 'Cover_Type'}
             df = pd.DataFrame(data)
-            
-            # Standardize the input features using the loaded scaler
-            df_scaled = scaler.transform(df)
-            
             # Make predictions using the model
-            prediction = model.predict(df_scaled)
+            prediction = model.predict(df)
             result = f"Predicted Cover Type: {prediction[0]}"
             return render_template('index.html', result=result)
         except Exception as e:
